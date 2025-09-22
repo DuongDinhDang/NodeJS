8bca30e87234f713975e934d04b0259de2da7b2a
DuongDinhDang <duongdinhdang10@gmail.com>
2025-09-22 23:39:38 +0700

    video 26

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World Update!");
 });
 
 app.listen(PORT, () => {
