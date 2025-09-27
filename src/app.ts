a05c8d934491448388315cec298e26c594a7cf99
DuongDinhDang <duongdinhdang10@gmail.com>
2025-09-27 12:35:10 +0700

    tao database, hien thi database, tao moi user, setup absolute import

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
-// const express = require("express");
 import express from "express";
 import "dotenv/config";
+import webRoutes from "./routes/web";
 const app = express();
 const PORT = process.env.PORT || 8080;
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+
+// config view engine
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+// config req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// config static files
+app.use(express.static("public"));
+
+// config router
+webRoutes(app);
 
 app.listen(PORT, () => {
     console.log(`My app is running on port: ${PORT} `);
-    console.log("env port: ", process.env.PORT);
 });
---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { getAllUsers, handleCreateUser } from "services/user.service";
+
+const getHomePage = async (req: Request, res: Response) => {
+    // get users
+    const users = await getAllUsers();
+    return res.render("home", {
+        users: users,
+    });
+};
+
+const getCreateUserPage = (req: Request, res: Response) => {
+    return res.render("create-user");
+};
+
+const postCreateUser = async (req: Request, res: Response) => {
+    const { fullName, email, address } = req.body;
+
+    // handle create user
+    await handleCreateUser(fullName, email, address);
+
+    return res.redirect("/");
+};
+
+export { getHomePage, getCreateUserPage, postCreateUser };
